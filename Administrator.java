19e38e20603bfe2156c8983ee8e2f5f19850164e
loveleen <loveleen.nancy@gmail.com>
2018-06-13 01:16:16 +0530

    Admin

--- Administrator.java
+++ Administrator.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 class Administrator
-{
+{	
 	String adminName;
 	String email;
 
+	Scanner input=new Scanner(System.in);
+
 	void updateCatalog()
 	{
+		System.out.println("Enter Admin Name");
+		adminName=input.nextLine();
 
+		System.out.println("Enter Email");
+		email=input.nextLine();
 	}
 }
