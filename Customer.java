c3cfd5ea7d6960ec43dd43dd577afb5b6c3c2e46
loveleen <loveleen.nancy@gmail.com>
2018-06-12 23:34:48 +0530

    Customer Setup

--- Customer.java
+++ Customer.java
@@ -1,23 +1,57 @@
+import java.util.Scanner;
+
 class Customer
 {
 	String customerName;
 	String address;
 	String email;
 	String creditCardInfo;
-	String ShippingInfo;
+	String shippingInfo;
+
+	Scanner input=new Scanner(System.in);
 
 	void register()
 	{
+		System.out.println("Enter Customer Name");
+		customerName=input.nextLine();
 
+		System.out.println("Enter Address");
+		address=input.nextLine();
+
+		System.out.println("Enter Email");
+		email=input.nextLine();
+
+		System.out.println("Enter Credit Card Information");
+		creditCardInfo=input.nextLine();
+
+		System.out.println("Enter Shipping Information");
+		shippingInfo=input.nextLine();
 	}
 
 	void login()
 	{
+		System.out.println("Enter Email");
+		email=input.nextLine();
 
+		System.out.println("Enter Credit Card Information");
+		creditCardInfo=input.nextLine();
 	}
 
 	void updateProfile()
 	{
+		System.out.println("Enter Customer Name");
+		customerName=input.nextLine();
 
+		System.out.println("Enter Address");
+		address=input.nextLine();
+
+		System.out.println("Enter Email");
+		email=input.nextLine();
+
+		System.out.println("Enter Credit Card Information");
+		creditCardInfo=input.nextLine();
+
+		System.out.println("Enter Shipping Information");
+		shippingInfo=input.nextLine();
 	}
 }
--- MainClass.java
+++ MainClass.java
@@ -2,17 +2,45 @@ import java.util.Scanner;
 
 class MainClass
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
-		int choice;
 		Scanner input=new Scanner(System.in);
-		System.out.println("Enter yout choice ");
-		System.out.println(" 1. Customer \n 2. Orders \n 3. Order Deatils \n 4. Shipping Information \n 5. Shopping Cart \n 6. User \n 7. Administrator");
+		int choice;		
+		System.out.println("Enter your choice ");
+		System.out.println(" 1. Customer \n 2. Order \n 3. Order Deatils \n 4. Shipping Information \n 5. Shopping Cart \n 6. User \n 7. Administrator");
 		choice=input.nextInt();
 		switch(choice)
 		{
-			case 1: System.out.println("A");
+			case 1: Customer c=new Customer();
+					c.register();
 					break;
+
+			case 2: Order o=new Order();
+					o.placeOrder();
+					break;
+			
+			case 3: OrderDetails od=new OrderDetails();
+					od.calcPrice();
+					break;
+
+			case 4: ShippingInfo si=new ShippingInfo();
+					si.updateShippingInfo();
+					break;
+
+			case 5: ShoppingCart sc=new ShoppingCart();
+					sc.addCartItem();
+					break;
+
+			case 6: User u=new User();
+					u.verifyLogin();
+					break;
+
+			case 7: Administrator a=new Administrator();
+					a.updateCatalog();
+					break;
+
+			default: System.out.print("Invalid Choice");
+		input.close();
 		}
 	}
 }
